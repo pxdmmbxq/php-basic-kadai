96e08c0a3c78c016d3a26ea446c87804be742721
pxdmmbxq <px.o1nulo.xd@gmail.com>
2025-05-17 12:11:15 +0900

    Update sort.php

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -10,26 +10,29 @@
     <p>
         <?php
         $order = ['昇順','降順'];
-        foreach ($order as $array){
-            if($array === '昇順'){
-                echo '昇順にソートします。.<br>';
-            
-                $nums = [15, 4, 18, 23, 10 ];
-                sort($nums);
-                foreach ($nums as $num){
-                    echo $num.'<br>';
+        function sort_2way($array,$order){
+                    if($order == '昇順'){
+                    echo '昇順にソートします。<br>';
+                    sort($array);
+                    foreach ($array as $num){
+                        echo $num.'<br>';
+                    }
+                }                
+                else{
+                    echo '降順にソートします。<br>';
+                    arsort($array);
+                    foreach ($array as $num){
+                        echo $num.'<br>';
+                    }
                 }
-            }                
-            else{
-                echo '降順にソートします。.<br>';
-            
-                $nums = [15, 4, 18, 23, 10 ];
-                arsort($nums);
-                foreach ($nums as $num){
-                    echo $num.'<br>';
-                }
-            }
-        }       
+                   
+        }
+        $nums = [15, 4, 18, 23, 10 ];
+        sort_2way($nums,'昇順');
+        echo '<br>';
+        sort_2way($nums,'降順');
+        echo '<br>';
+
         ?>
     </p>
 </body>
